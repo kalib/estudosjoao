88160e29b7f70e74d37dc4e4fcdeaf534b27d99d
Marcelo Rocha <mrocha@dragos.com>
2022-12-26 18:53:33 -0500

    Exercício 04 adicionado

--- README.md
+++ README.md
@@ -64,6 +64,7 @@ Badges retirados de: https://github.com/Ileriayo/markdown-badges
 * ![Visual Studio Code](https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white)
 * ![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white)
 * ![Git](https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white)
+* ![GitKraken](https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white)
 * ![Manjaro](https://img.shields.io/badge/Manjaro-35BF5C?style=for-the-badge&logo=Manjaro&logoColor=white)
 
 
---
+++ Scripts/Exercicios/comidas.sh
@@ -0,0 +1,20 @@
+# Crie um script que execute as seguintes instruções:
+#
+# 1- Imprima na tela: Bem vindo ao mundo da comida;
+# 2- Pergunte ao usuário: "Em primeiro lugar, qual o seu nome?"
+# 3- Armazene o nome digitado pelo usuário em uma variável chamada *nome*;
+# 4- Imprima na tela: "Seja bem vindo ao mundo da comida, <nome do usuário>!"
+# 5- Faça o script esperar 2 segundos
+# 6- Pergunte ao usuário: "Qual a sua comida favorita?"
+# 7- Salve o valor em uma variável chamada *comida_usuario*;
+# 8- Aguarde 2 segundos
+# 9- Imprima: "Interessante. Eu não gosto muito de <comida que o usuário digitou>."
+# 10- Aguarde 2 segundos
+# 11- Imprima: "Qual a sua segunda comida favorita?"
+# 12- Armazene a segunda comida em uma variável chamada *comida_usuario_2*;
+# 13- Aguarde mais 2 segundos
+# 14- Imprima: "Eu acho que a mistura de <nome da comida preferida> com < nome da segunda comida preferida> ficaria estranho"
+# 15- Aguarde 1 segundo
+# 16- Imprima: "Você é estranho <nome do usuario>, comendo <nomedaprimeira comida preferida> misturado com <nome da segunda comida favorita>"
+# 17- Aguarde 2 segundos
+# 18- Imprima: "Depois dessa, encerro a nossa conversa. Adeus <nome da pessoa>"
