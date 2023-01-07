3f95d5bac68e7e19ee2a8372c214daa3433973bf
João <arieljoaoariel@gmail.com>
2023-01-07 19:40:53 -0300

    primeiro script python e novo diretorio criado

---
+++ Scripts/python/ola-mundo.py
@@ -0,0 +1,6 @@
+print('olá, me diga seu nome, idade e peso\n')
+nome = input('qual seu nome? ')
+idade = int(input('qual sua idade? '))
+peso = float(input('qual seu peso? '))
+print('olá,', nome, 'sua idade é', idade, 'seu peso é', peso)
+print('a soma do seu peso com a idade é', peso + idade )
