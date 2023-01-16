f1f036d7b3c461b6713f9ab2a78f772d96e03f72
Marcelo Rocha <mrocha@dragos.com>
2023-01-16 10:15:58 -0500

    Exercício 04 adicionado

---
+++ Scripts/python/Exercicios/exercicio_04.py
@@ -0,0 +1,26 @@
+"""
+Vamos aproveitar este exercício para aprender algo novo.
+Comentários de muitas linhas. Ao invés de colocar um # no começo de cada linha
+de comentário, podemos simplesmente utilizar 3 aspas duplas no começo e novmaente
+3 aspas duplas no fim do bloco. E tudo o que existir entre esas 3 aspas duplas
+seá considerado pelo Python como um comentário.
+
+Exercício 04:
+
+Crie um programa que calcula a média de notas de um aluno e diga se ele passou de
+ano ou não. Parecido com o que fizemos em Bash, porém desta vez em Python:
+- O programa deverá perguntar se a pessoa é aluno ou professor:
+   - Se for aluno, o programa dará uma mensagem dizendo que apenas professores
+   podem executar o sistema e encerra.
+   - Se for um professor, o programa segue adiante;
+- Pergunta o nome do professor;
+- Pergunta o nome do aluno para o qual a média será calculada;
+- Recebe 3 variáveis, cada uma delas será uma nota: N1, N2, N3
+- Crie uma variável MEDIA que será o resultado da soma das 3 notas dividido por 3.
+   - Exemplo: MEDIA = (N1 + N2 + N3) / 3
+- Validação da média:
+   - Se a média for maior ou igual a 7.0, imprima uma mensagem dizendo que o aluno
+   passou de ano.
+   - Se a média for menor que 7.0 imprima uma mensagem dizendo que o aluno não passou
+   de ano.
+"""
