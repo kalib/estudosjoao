a6bfda69442653aa5613e5aca10aa4f8ee02dec9
João <arieljoaoariel@gmail.com>
2023-01-17 10:11:54 -0300

    exercicio 4 de python feito

--- Scripts/python/Exercicios/exercicio_04.py
+++ Scripts/python/Exercicios/exercicio_04.py
@@ -24,3 +24,40 @@ ano ou não. Parecido com o que fizemos em Bash, porém desta vez em Python:
    - Se a média for menor que 7.0 imprima uma mensagem dizendo que o aluno não passou
    de ano.
 """
+
+resposta = input('você é aluno ou professor? ')
+
+if resposta == 'aluno':
+   print('apenas professores podem usar isso')
+   exit
+
+if resposta == 'professor':
+   print('olá, professor!')
+   
+   nome_prof = input('professor, qual seu nome? ')
+   nome_aluno = input('ok, {}, me diga o nome do aluno em que a média vai ser calculada! '.format(nome_prof))
+   
+   N1 = float(input('me diga a primeira nota do {} '.format(nome_aluno)))
+   N2 = float(input('me diga a segunda nota do aluno {} '.format(nome_aluno)))
+   N3 = float(input('me diga a ultima e terceira nota do aluno {} '.format(nome_aluno)))
+   
+   media = (N1 + N2 +N3) / 3
+   
+if N1 > 10:
+   print('notas maiores que 10 não são permitidas')
+   exit
+
+if N2 > 10:
+   print('notas maiores que 10 não são permitidas')
+   exit
+
+if N3 > 10:
+   print('notas maiores que 10 não são permitidas')
+   exit
+
+if media > 6:
+   print('o aluno {} passou de ano!!! parabens!'.format(nome_aluno))
+
+if media < 6:
+   print('o aluno {} infelizmente não passou...'.format(nome_aluno))
+   
