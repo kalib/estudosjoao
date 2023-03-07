a0a31aed488926d6a4f430fd8b3b3b851134c0a3
João <arieljoaoariel@gmail.com>
2023-03-07 17:54:44 -0300

    novos exercicios feitps

---
+++ CursoYT-python/Exercicios/Praticar/Exercicios/soma.py
@@ -0,0 +1,4 @@
+n1 = float(input('me diga um número'))
+n2 = float(input('me diga outro numero'))
+soma = n1 + n2
+print('a soma do numero {} e do numero {} é igual a: {}'.format(n1,n2,soma))
---
+++ CursoYT-python/Exercicios/Praticar/Exercicios/tipo.py
@@ -0,0 +1,2 @@
+resposta = input('me diga qualquer coisa, um numero ou palavra')
+print(type(resposta))
